180e294b49f149de0afac85f36e95e3668d5bf2f
Alexander <aleks_bs95@abv.bg>
2016-04-23 15:05:50 +0300

    multi threading

--- KnokKnok/src/KKMultiServer.java
+++ KnokKnok/src/KKMultiServer.java
@@ -52,4 +52,4 @@ public class KKMultiServer {
             System.exit(-1);
         }
     }
-}
+}	
--- KnokKnok/src/KnockKnockServer.java
+++ KnokKnok/src/KnockKnockServer.java
@@ -1,71 +1,73 @@
-import javax.net.ssl.SSLServerSocket;
-import javax.net.ssl.SSLServerSocketFactory;
-import javax.net.ssl.SSLSocket;
+/*
+ * Copyright (c) 1995, 2014, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */ 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.net.*;
+import java.io.*;
 
 public class KnockKnockServer {
-    public static void main(String[] arstring) {
-        try {
-            SSLSocket sslsocket = createServerSocket();
-            BufferedReader bufferedClientReader = readClientData(sslsocket);
-            BufferedReader bufferedConsoleReader = readFromConsole();
-            writeToConsole(bufferedClientReader);
-            sendDataToClient(sslsocket, bufferedConsoleReader);
+    public static void main(String[] args) throws IOException {
+        
+        if (args.length != 1) {
+            System.err.println("Usage: java KnockKnockServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try ( 
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+        
+            String inputLine, outputLine;
             
-        } catch (Exception exception) {
-            exception.printStackTrace();
+            // Initiate conversation with client
+            KnockKnockProtocol kkp = new KnockKnockProtocol();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
         }
     }
-    
-    private static void sendDataToClient(SSLSocket sslsocket, BufferedReader bufferedreader) throws IOException {
-        OutputStream outputstream = sslsocket.getOutputStream();
-        OutputStreamWriter outputstreamwriter = new OutputStreamWriter(outputstream);
-        BufferedWriter bufferedWriter = new BufferedWriter(outputstreamwriter);
-		String string = null;
-		while ((string = bufferedreader.readLine()) != null) {
-			System.out.println(string);
-		    bufferedWriter.write(string + '\n');
-		    bufferedWriter.flush();
-		}
-	}
-    
-    private static BufferedReader readFromConsole() {
-		InputStream inputstream = System.in;
-        InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
-        BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
-		return bufferedreader;
-	}
-    
-	private static BufferedReader readClientData(SSLSocket sslsocket)
-			throws IOException {
-		InputStream inputstream = sslsocket.getInputStream();
-		InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
-		BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
-		return bufferedreader;
-	}
-
-	private static SSLSocket createServerSocket() throws IOException {
-		SSLServerSocketFactory sslserversocketfactory =
-		        (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-		SSLServerSocket sslserversocket =
-		        (SSLServerSocket) sslserversocketfactory.createServerSocket(9999);
-		SSLSocket sslsocket = (SSLSocket) sslserversocket.accept();
-		return sslsocket;
-	}
-
-	private static void writeToConsole(BufferedReader bufferedreader)
-			throws IOException {
-		String string = null;
-		while ((string = bufferedreader.readLine()) != null) {
-		    System.out.println(string);
-		    System.out.flush();
-		}
-	}
 }
